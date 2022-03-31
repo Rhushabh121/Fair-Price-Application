a93777e8b9efb9abc6087692f450b9ec204e65b8
Kushaan Kashyap <kashyapk@myumanitoba.ca>
2022-03-31 16:55:12 -0500

    updating the branch with working database added

--- app/src/main/java/comp3350/fairprice/presentation/Register.java
+++ app/src/main/java/comp3350/fairprice/presentation/Register.java
@@ -1,56 +1,51 @@
 package comp3350.fairprice.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import comp3350.fairprice.R;
-import comp3350.fairprice.presentation.Welcome;
 import comp3350.fairprice.presentation.HomepageActivity;
+import comp3350.fairprice.presentation.Welcome;
 
 
-
-
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_register);
     }
 
-
     /**
-     * Authenticate the user when the "Sign in" button is clicked.
-     * Ensure username exists and the corresponding password matches
-     * If user is authenticated
+     * Validate the user when the "Create Account" button is clicked.
+     * Ensure username does not exist in database
+     * If user is valid
      *      take them to welcome page
      * Else
      *      Display error
      * @param view
      */
-    public void authenticate(View view) {
+    public void validate(View view) {
 
         Intent intent = new Intent(this, Welcome.class);
 
         // get inputs from username and password fields
-        EditText username = (EditText) findViewById(R.id.editTextTextPersonName);
-        EditText password = (EditText) findViewById(R.id.editTextTextPersonName3);
+        EditText username = (EditText) findViewById(R.id.editTextTextPersonName4);
+        EditText password = (EditText) findViewById(R.id.editTextTextPersonName5);
         String message = "";
         if(username.length() != 0 && password.length() != 0) {
-             message = "Username: \t"+username.getText().toString()+"\nPassword: \t"+password.getText().toString();
-
+             message = "Username: \t" + username.getText().toString() + "\nPassword: \t"
+                    + password.getText().toString() + "\n\nYour account has been successfully created.";
         }
         else {
-            message = "Unable to login the account.\nUsername or password does not exist.";
+            message = "Unable to create the account.\nUsername and password did not meet specification";
         }
-
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
@@ -66,4 +61,7 @@ public class Login extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+
+
 }
