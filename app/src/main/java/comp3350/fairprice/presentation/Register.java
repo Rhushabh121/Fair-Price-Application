71d29c69e0f87f741a405c688f909d32c206cab4
Kushaan Kashyap <kashyapk@myumanitoba.ca>
2022-04-11 23:23:57 -0500

    Register.java updated with correct version in feature_Login branch with regards to DB

--- app/src/main/java/comp3350/fairprice/presentation/Register.java
+++ app/src/main/java/comp3350/fairprice/presentation/Register.java
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 import comp3350.fairprice.R;
+import comp3350.fairprice.business.AccessUsers;
+import comp3350.fairprice.objects.User;
 import comp3350.fairprice.presentation.HomepageActivity;
 import comp3350.fairprice.presentation.Welcome;
 
 
 public class Register extends AppCompatActivity {
 
+    private AccessUsers accessUsers;
+    private List<User> userList;
+    private ArrayAdapter<User> userArrayAdapter;
+
+
+    public  static int userCount = 0;
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
@@ -23,7 +35,8 @@ public class Register extends AppCompatActivity {
     }
 
     /**
-     * Validate the user when the "Create Account" button is clicked.
+     * Validate the user when the "Create Account" button is clicked in  activity_register.xml.
+     *
      * Ensure username does not exist in database
      * If user is valid
      *      take them to welcome page
@@ -31,16 +44,44 @@ public class Register extends AppCompatActivity {
      *      Display error
      * @param view
      */
-    public void validate(View view) {
+    public void registerUser(View view) {
+
+        // --------------- DATABASE Changes -----------------
+
+        User user = createUserFromEditText();
+        String result;
+
+        result = validateUserData(user, true);
+
+        if (result == null)
+        {
+            try {
+                user = accessUsers.addUser(user);
+                userList = accessUsers.getUsers();
+                userArrayAdapter.notifyDataSetChanged();
+
+                int pos = userList.indexOf(user);
+
+
+            } catch (final Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        else
+        {
+            System.out.println(result);
+        }
 
         Intent intent = new Intent(this, Welcome.class);
 
         // get inputs from username and password fields
         EditText username = (EditText) findViewById(R.id.editTextTextPersonName4);
         EditText password = (EditText) findViewById(R.id.editTextTextPersonName5);
+
         String message = "";
         if(username.length() != 0 && password.length() != 0) {
-             message = "Username: \t" + username.getText().toString() + "\nPassword: \t"
+            message = "Username: \t" + username.getText().toString() + "\nPassword: \t"
                     + password.getText().toString() + "\n\nYour account has been successfully created.";
         }
         else {
@@ -50,6 +91,45 @@ public class Register extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+
+    /**
+     * Create a user object when the create account button is clicked in activity_register and
+     *
+     * @return User object is returned
+     */
+    private User createUserFromEditText() {
+
+        // get inputs from the fields required for registration of user
+        EditText username = (EditText)findViewById(R.id.editTextTextPersonName4);
+        EditText password = (EditText)findViewById(R.id.editTextTextPersonName5);
+
+
+        User user = new User(Integer.toString(userCount), username.getText().toString(), "", password.getText().toString(), "");
+        userCount++;
+        return user;
+    }
+
+
+    /**
+     *
+     * @param user
+     * @param isNewUser
+     * @return
+     */
+    private String validateUserData(User user, boolean isNewUser) {
+        if (user.getName().length() == 0) {
+            return "Username required";
+        }
+
+        if (user.getPassword().length() == 0) {
+            return "Password required";
+        }
+
+        return null;
+    }
+
+
 
     /**
      * this function on button click will open the main
