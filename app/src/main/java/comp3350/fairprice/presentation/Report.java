fc4132115c7bdb6c21be68559c73dd6e3d2ff591
Jack <trungn@myumanitoba.ca>
2022-04-26 05:19:22 +0700

    comments added

--- app/src/main/java/comp3350/fairprice/presentation/Report.java
+++ app/src/main/java/comp3350/fairprice/presentation/Report.java
@@ -9,6 +9,7 @@ import android.view.View;
 import comp3350.fairprice.R;
 
 public class Report extends AppCompatActivity {
+// This class mainly shows the policy of app for reporting , not actually a report
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,7 @@ public class Report extends AppCompatActivity {
         setContentView(R.layout.activity_report);
     }
 
+    // This function will guide to the report page
     public void continues(View v){
         Intent i = new Intent(this, reportPage.class);
         startActivity(i);
--- app/src/main/java/comp3350/fairprice/presentation/reportPage.java
+++ app/src/main/java/comp3350/fairprice/presentation/reportPage.java
@@ -15,24 +15,27 @@ import comp3350.fairprice.R;
 import comp3350.fairprice.databinding.ActivityMainBinding;
 
 public class reportPage extends AppCompatActivity {
-    ActivityMainBinding binding;
+    // Main report page where users will select options for reporting a listing
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_page);
     }
 
+    // After choosing an report option activity
     public void doneReport(View v){
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, MainActivity.class); // will return user to the main page
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        AlertDialog.Builder builders = new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this); // Pop Up window for confirmation
+        AlertDialog.Builder builders = new AlertDialog.Builder(this); // Pop Up window for notifying success report
 
         builder.setCancelable(true);
         builder.setTitle("You want to report this listing for this reason ?");
         builder.setMessage("Confirmation on reporting Listing");
 
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            // Pressing Cancel will cancel the Pop Up window and still stay in this page .
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.cancel();
@@ -42,7 +45,7 @@ public class reportPage extends AppCompatActivity {
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                // Pressing OK will result in Pop Up message notifying success report and lead back to the main page
                 builders.setTitle("Report Success");
                 builders.setNeutralButton("OK", new DialogInterface.OnClickListener() {
                     @Override
